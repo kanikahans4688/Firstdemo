6cc1e3a085c1e3fa559f517f7bb950c3f0ce13f3
kanikahans4688 <you@example.com>
2020-06-01 15:19:58 +1000

    first maven project added

---
+++ src/main/java/com/qa/base/Baseclass.java
@@ -0,0 +1,10 @@
+package com.qa.base;
+
+public class Baseclass {
+
+	
+	public void baseset() {
+		
+		System.out.println("base class");
+	}
+}
---
+++ src/main/java/com/qa/page/Loginpage.java
@@ -0,0 +1,10 @@
+package com.qa.page;
+
+public class Loginpage {
+	
+	public void loginsetup() {
+		
+		System.out.println("log in set up");
+	}
+
+}
